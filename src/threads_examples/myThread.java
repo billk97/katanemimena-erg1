7ff5ff384b2891e7a3ea09143d2701d58699790e
billk97 <bill1897@yahoo.gr>
2019-02-25 17:43:59 +0200

    1

---
+++ src/threads_examples/myThread.java
@@ -0,0 +1,21 @@
+package threads_examples;
+
+public class myThread extends  Thread{
+    String input;
+    private myThread (String input) {this.input = input;}
+    public void run()
+    {
+        for(int i=0; i<10; i++){
+            System.err.println(i+":\t"+input);
+            try{
+                sleep((int)(Math.random()*500));
+            }catch (InterruptedException e){}
+        }
+    }
+    public static void main(String [] args){
+        Thread t = new myThread("Distributed");
+        Thread t2 = new myThread("Systems");
+        t.start();
+        t2.start();
+    }
+}
